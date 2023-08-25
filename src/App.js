d4d14854a16873d267b772ec61edc6efac85c8e6
XZACTA <110074271+XZACTA@users.noreply.github.com>
2023-08-25 15:06:04 +0700

    Create refs-array
    Create refs-array

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {Calculator} from './calculator'
 import Banner from './banner'
 import { EventData2 } from './even-data'
 import RefsFunc from './refs-func'
+import RefsArray from './refs-array'
 
 export default function App(){
-  return <RefsFunc/>
+  return <RefsArray/>
 }

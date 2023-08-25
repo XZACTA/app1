ab9fde17b1b373996c19bf1162104c96b453ce34
XZACTA <110074271+XZACTA@users.noreply.github.com>
2023-08-25 15:43:42 +0700

    Create state-func.js
    Create state-func.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import Banner from './banner'
 import { EventData2 } from './even-data'
 import RefsFunc from './refs-func'
 import RefsArray from './refs-array'
+import MessageBox from './state-func'
+
 
 export default function App(){
-  return <RefsArray/>
+  return (
+    <>
+    <div>
+      <RefsArray/>
+      <MessageBox/>
+    </div>
+    </>
+  )
 }

94a43b815e2aa1591f31861a262b0decdaceacf3
XZACTA <110074271+XZACTA@users.noreply.github.com>
2023-08-25 14:09:25 +0700

    Creat refs-func
    Creat refs-func

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {Calculator} from './calculator'
 import Banner from './banner'
 import { EventData2 } from './even-data'
+import RefsFunc from './refs-func'
 
 export default function App(){
-  return <EventData2/>
+  return <RefsFunc/>
 }

e6de533c9e54fa3f219fa3729a798cec3390d3cf
XZACTA <110074271+XZACTA@users.noreply.github.com>
2023-09-08 15:37:39 +0700

    Add Content2 and Header2
    Add Content2 and Header2

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import React from 'react'
 // import RefsArray from './refs-array'
 // import MessageBox from './state-func'
 import { userContext } from './context'
-import Header from './context-header'
+// import Header from './context-header'
+// import Content from './context-content'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
 
 export default function App(){
+  let[user,setUser]= React.useState()
   return (
-      <userContext.Provider value={'Tom Jerry'}>
-        <Header/>
+      <userContext.Provider value={[user,setUser]}>
+        <Header2/>
+        <Content2/>
       </userContext.Provider>
   )
 }

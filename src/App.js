988169328596e9ffae861ec78c2247ef7bed11e1
XZACTA <110074271+XZACTA@users.noreply.github.com>
2023-09-08 14:38:49 +0700

    Create context and context-header
    Create context and context-header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import {Calculator} from './calculator'
-import Banner from './banner'
-import { EventData2 } from './even-data'
-import RefsFunc from './refs-func'
-import RefsArray from './refs-array'
-import MessageBox from './state-func'
+// import {Calculator} from './calculator'
+// import Banner from './banner'
+// import { EventData2 } from './even-data'
+// import RefsFunc from './refs-func'
+// import RefsArray from './refs-array'
+// import MessageBox from './state-func'
+import { userContext } from './context'
+import Header from './context-header'
 
 
 export default function App(){
   return (
-    <>
-    <div>
-      <RefsArray/>
-      <MessageBox/>
-    </div>
-    </>
+      <userContext.Provider value={'Tom Jerry'}>
+        <Header/>
+      </userContext.Provider>
   )
 }

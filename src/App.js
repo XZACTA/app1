242dd8af6ebabe8aa225def7a13ef19818cef2c9
XZACTA <110074271+XZACTA@users.noreply.github.com>
2023-09-22 15:32:12 +0700

    Route and NavLink
    add router3.js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,18 @@ import { userContext } from './context'
 // import Content from './context-content'
 import Header2 from './context-header2'
 import Content2 from './context-content2'
+//import Router2 from './router2'
+import Router3 from './router3'
 
 
 export default function App(){
-  let[user,setUser]= React.useState()
-  return (
-      <userContext.Provider value={[user,setUser]}>
-        <Header2/>
-        <Content2/>
-      </userContext.Provider>
-  )
+  // let[user,setUser]= React.useState()
+  // return (
+  //     <userContext.Provider value={[user,setUser]}>
+  //       <Header2/>
+  //       <Content2/>
+  //     </userContext.Provider>
+  // )
+
+  return<Router3/>
 }
---
+++ src/member.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Member() {
+  return (
+    <div style={{textAlign:'center'}}>Member Page</div>
+  )
+}
+
+export default Member

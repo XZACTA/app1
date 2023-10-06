0f908806be4493fe3a7fec746ce4fdf9fd6d7266
XZACTA <110074271+XZACTA@users.noreply.github.com>
2023-10-06 15:28:32 +0700

    Form-validation.js
    Form-validation.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import Content2 from './context-content2'
 import Router3 from './router3'
 import FormText from './form-text'
 import FormCheck from './form-check'
-
+import FormValidation from './form-validation'
 
 
 export default function App(){
@@ -28,5 +28,6 @@ export default function App(){
 
   //return<Router3/>
   //return<FormText/>
-  return<FormCheck/>
+  //return<FormCheck/>
+  return<FormValidation/>
 }
